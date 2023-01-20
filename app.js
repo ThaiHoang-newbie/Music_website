b3088ca997c868ed32a0b16f817e1cb6dc1265ff
ThaiHoang-newbie <thaihoang20112k3@gmail.com>
2023-01-20 08:30:10 +0700

    Tai search ả

--- app.js
+++ app.js
@@ -81,12 +81,56 @@ const songs = [
         songName: `Lut Gaye <br><div class="subtitle">Jubin Nautiyal</div>`,
         poster: "img/15.jpg",
     },
+  
 ]
 
 Array.from(document.getElementsByClassName('songItem')).forEach((element, i)=>{
     element.getElementsByTagName('img')[0].src = songs[i].poster;
     element.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
+});
+
+// search data start
+let search_results = document.getElementsByClassName('search_results')[0];
+songs.forEach(element => {
+    const {id,songName,poster} =element;
+    // console.log(poster);
+    let card  = document.createElement('a');
+    card.classList.add('card');
+    card.href= "#" + id;
+
+    card.innerHTML = `
+    <img src="${poster}" alt="">
+                            <div class="content">
+                               ${songName}
+                            </div>
+    `;
+    search_results.appendChild(card);
+
+});
+
+let input =document.getElementsByTagName('input')[0];
+input.addEventListener('keyup',()=>{
+    let input_value=input.value.toUpperCase();
+    let items = search_results.getElementsByTagName('a');
+
+    for (let index =0 ;index< items.length;index++){
+        let as = items[index].getElementsByClassName('content')[0];
+        let text_value = as.textContent || as.innerHTML;
+
+
+        if (text_value.toUpperCase().indexOf(input_value)  > -1) {
+            items[index].style.display = "flex";
+        } else {
+            items[index].style.display = "none";
+            
+        }
+
+
+    
+    }
 })
+// search data end
+
 
 
 let masterPlay = document.getElementById('masterPlay');
